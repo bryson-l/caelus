db01ea414ec698995bca3701c86df98f715d53f9
Bryson Longoria <bryson326@gmail.com>
2019-07-08 11:20:18 -0400

    got bootstrap working

---
+++ caelus/src/app/test-control/test.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit, Input } from '@angular/core';
+
+@Component({
+  selector: 'test',
+  templateUrl: './test.component.html'
+})
+export class TestComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
