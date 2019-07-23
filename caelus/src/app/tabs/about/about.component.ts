095c70838539f20bad0cf0c03a65b42d82f525f4
Bryson <bryson326@gmail.com>
2019-07-23 06:14:11 -0400

    got the widgets to display different data

--- caelus/src/app/tabs/about/about.component.ts
+++ caelus/src/app/tabs/about/about.component.ts
@@ -1,25 +1,50 @@
-import { Component, ViewChild } from '@angular/core';
+import { Component, ViewChild, OnInit } from '@angular/core';
 import { DxDataGridComponent } from 'devextreme-angular/ui/data-grid';
 
 @Component({
   selector: 'about',
   templateUrl: './about.component.html'
 })
-export class AboutComponent {
+export class AboutComponent implements OnInit {
+
+  ngOnInit() {
+    setTimeout(() => {
+      this.grid.instance.selectRowsByIndexes(0)
+    }, 20)
+  }
 
   testDataCurrent: any[] = [
-    {to: 'Raleigh (RDU)', time: '8:00 am', id: 1},
-    {to: 'Atlanta (ATL)', time: '9:30 am', id: 2}
+    {to: 'Raleigh (RDU)', from: 'Raleigh (RDU)', time: '8:00 am', id: 1},
+    {to: 'Atlanta (ATL)', from: 'Raleigh (RDU)', time: '9:30 am', id: 2}
   ]
 
+  testDataTrade: any[] = [
+    {to: 'Dallas/Fort Worth (DFW)', from: 'Raleigh (RDU)', time: '9:30 am', id: 3}
+  ]
+
+  currentSelected: any = {};
+  tradeSelected: any = {};
+
   @ViewChild('grid', {static: false}) grid: DxDataGridComponent
+  @ViewChild('grid2', {static: false}) grid2: DxDataGridComponent
 
   onRowSelect(e: any) {
     console.log(e)
   }
 
+  onCurrentSelect(e: any) {
+    // e.key is the row's object
+    this.currentSelected = e.key
+  }
+
+  onTradeSelect(e: any) {
+    this.tradeSelected = e.key
+  }
+
   onTradeClick() {
-    
+    if (this.currentSelected && this.tradeSelected) {
+
+    }
   }
 
 }
