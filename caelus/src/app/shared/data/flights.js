4cf38b87c7c3ecf628c131d941b390289f0ab477
Bryson Longoria <bryson326@gmail.com>
2019-08-09 22:45:01 -0400

    no idea what this actually did

--- caelus/src/app/shared/data/flights.js
+++ caelus/src/app/shared/data/flights.js
@@ -1,10 +1,17 @@
 module.exports = [
     {"flight_id": 1, "start": "RDU", "end": "DEN", "departure_time": new Date('2019/08/08 08:00:00'), "arrival_time": new Date('2019/08/08 12:00:00')},
     {"flight_id": 2, "start": "DEN", "end": "DFW", "departure_time": new Date('2019/08/08 14:00:00'), "arrival_time": new Date('2019/08/08 16:30:00')},
+
     {"flight_id": 3, "start": "DFW", "end": "CLT", "departure_time": new Date('2019/08/09 09:30:00'), "arrival_time": new Date('2019/08/09 12:00:00')},
     {"flight_id": 4, "start": "CLT", "end": "RDU", "departure_time": new Date('2019/08/09 13:00:00'), "arrival_time": new Date('2019/08/09 16:30:00')},
+
     {"flight_id": 5, "start": "RDU", "end": "JFK", "departure_time": new Date('2019/08/10 08:00:00'), "arrival_time": new Date('2019/08/10 10:30:00')},
     {"flight_id": 6, "start": "JFK", "end": "LAX", "departure_time": new Date('2019/08/10 11:00:00'), "arrival_time": new Date('2019/08/10 14:45:00')},
     {"flight_id": 7, "start": "LAX", "end": "ATL", "departure_time": new Date('2019/08/10 15:30:00'), "arrival_time": new Date('2019/08/10 19:30:00')},
-    {"flight_id": 8, "start": "ATL", "end": "ORD", "departure_time": new Date('2019/08/10 20:00:00'), "arrival_time": new Date('2019/08/10 22:00:00')}
+    {"flight_id": 8, "start": "ATL", "end": "ORD", "departure_time": new Date('2019/08/10 20:00:00'), "arrival_time": new Date('2019/08/10 22:00:00')},
+
+    {"flight_id": 9, "start": "RDU", "end": "DEN", "departure_time": new Date('2019/08/08 07:30:00'), "arrival_time": new Date('2019/08/08 11:30:00')},
+    {"flight_id": 10, "start": "RDU", "end": "JFK", "departure_time": new Date('2019/08/08 08:00:00'), "arrival_time": new Date('2019/08/08 11:00:00')},
+    {"flight_id": 11, "start": "RDU", "end": "DEN", "departure_time": new Date('2019/08/08 08:00:00'), "arrival_time": new Date('2019/08/08 12:00:00')}
+
 ]
--- caelus/src/app/shared/services/flight.service.ts
+++ caelus/src/app/shared/services/flight.service.ts
@@ -37,7 +37,7 @@ export class FlightService {
       let validTrades: Flight[] = []
       data.forEach(flight => {
         // need to write function to check if a trade conflicts with the rest of current schedule
-        if (this.conflictsWithSchedule(currentSchedule, flight, tradeFor)) {
+        if (this.isValidTrade(tradeFor, flight, currentSchedule)) {
             validTrades.push(flight)
         }
       })
@@ -48,30 +48,28 @@ export class FlightService {
           return null
       }
   }
-  
-  conflictsWithSchedule(currentSchedule: Flight[], tradeFor: Flight, tradeAway: Flight): boolean {
-      // tradeAway should be an existing element inside of currentSchedule
-      // first get the index for tradeAway
-      let tradeIndex = 0
-      for (let i = 0; i < currentSchedule.length; i++) {
-          if (currentSchedule[i] == tradeAway) {
-              tradeIndex = i
-              break
-          }
-      }
-      // pass in tradeFor, tradeAway, and currentSchedule[i + 1] into isValidTrade
-      return this.isValidTrade(tradeFor, tradeAway, currentSchedule[tradeIndex + 1])
-  }
-
-  isValidTrade(tradeFor: Flight, tradeAway: Flight, nextFlight: Flight): boolean { 
-      // checks if it trade for flight is within a certain window of time from the trade away flight
-      let minAhead: boolean = (moment(tradeAway.departure_time).add(15, 'minutes') > moment(tradeFor.departure_time))
-      let minBehind: boolean = (moment(tradeAway.departure_time).subtract(15, 'minutes') < moment(tradeFor.departure_time))
-      let timeForNextFlight: boolean = true
-      if (nextFlight) {
-          timeForNextFlight = (moment(tradeFor.arrival_time) < moment(nextFlight.departure_time).subtract(15, 'minutes'))
-      }
-      return (minAhead && minBehind && timeForNextFlight)
+  // need to redo this whole thing, there needs to be a check for a valid schedule vs a check for a valid trade.
+  // a valid trade does not need to align with the rest of the schedule. defeats purpose. just needs to be possible with the previous flight destination and arrival time
+  // valid schedule will be handled elsewhere
+  isValidTrade(tradeFor: Flight, tradeAway: Flight, currentSchedule: Flight[]): boolean {
+    let tradeIndex = 0
+    for (let i = 0; i < currentSchedule.length; i++) {
+        if (currentSchedule[i] == tradeAway) {
+            tradeIndex = i
+            break
+        }
+    }
+    let nextFlight = currentSchedule[tradeIndex + 1]
+    // checks if it trade for flight is within a certain window of time from the trade away flight
+    let minAhead: boolean = (moment(tradeAway.departure_time).add(15, 'minutes') > moment(tradeFor.departure_time))
+    let minBehind: boolean = (moment(tradeAway.departure_time).subtract(15, 'minutes') < moment(tradeFor.departure_time))
+    let timeForNextFlight: boolean = true
+    if (moment(nextFlight.departure_time).toDate() == moment(tradeFor.arrival_time).toDate()) {
+        timeForNextFlight = (moment(tradeFor.arrival_time) < moment(nextFlight.departure_time).subtract(15, 'minutes'))
+    }
+    // need to check that the arrival location matches the next flight departure location
+    let rightLocation: boolean = tradeFor.end == nextFlight.start
+    return (minAhead && minBehind && timeForNextFlight && rightLocation)
   }
 
 }
