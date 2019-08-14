a5f8757492f3bffc6c4ba4ca69b54f9ec5f89437
Bryson Longoria <bryson326@gmail.com>
2019-08-13 21:58:27 -0400

    formatting

--- caelus/src/app/shared/services/flight.service.ts
+++ caelus/src/app/shared/services/flight.service.ts
@@ -74,31 +74,4 @@ export class FlightService {
     return (sameStart && timeFromPrevFlight)
   }
   
-  // need to redo this whole thing, there needs to be a check for a valid schedule vs a check for a valid trade.
-  // a valid trade does not need to align with the rest of the schedule. defeats purpose. just needs to be possible with the previous flight destination and arrival time
-  // valid schedule will be handled elsewhere
-
-//   isValidTrade(tradeFor: Flight, tradeAway: Flight, currentSchedule: Flight[]): boolean {
-//     let tradeIndex = 0
-//     for (let i = 0; i < currentSchedule.length; i++) {
-//         if (currentSchedule[i] == tradeAway) {
-//             tradeIndex = i
-//             break
-//         }
-//     }
-//     let nextFlight = currentSchedule[tradeIndex + 1]
-//     // checks if it trade for flight is within a certain window of time from the trade away flight
-//     let minAhead: boolean = (moment(tradeAway.departure_time).add(15, 'minutes') > moment(tradeFor.departure_time))
-//     let minBehind: boolean = (moment(tradeAway.departure_time).subtract(15, 'minutes') < moment(tradeFor.departure_time))
-//     let timeForNextFlight: boolean = true
-//     if (moment(nextFlight.departure_time).toDate() == moment(tradeFor.arrival_time).toDate()) {
-//         timeForNextFlight = (moment(tradeFor.arrival_time) < moment(nextFlight.departure_time).subtract(15, 'minutes'))
-//     }
-//     // need to check that the arrival location matches the next flight departure location
-//     let rightLocation: boolean = tradeFor.end == nextFlight.start
-//     return (minAhead && minBehind && timeForNextFlight && rightLocation)
-//   }
-
-  
-
 }
